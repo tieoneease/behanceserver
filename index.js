aca9f998f67affff46b943312bb333a873db373f
Sam Chung <tieoneease@gmail.com>
2017-03-12 13:40:49 -0400

    endpoint

--- index.js
+++ index.js
@@ -33,17 +33,17 @@ function prettyPrint(json) {
 }
 
 app.get('/user', function (req, res) {
-  prettyPrint(cachedData)
-  res.send(cachedData)
-  //axios.get(endpointURL('/users/achen041bd2/projects'))
-    //.then(response => {
-      //console.dir(response.data)
-      //writeFile(fileURL, response.data)
-      //res.send(response.data)
-    //})
-    //.catch(error => {
-      //console.log('error', error)
-    //})
+  //prettyPrint(cachedData)
+  //res.send(cachedData)
+  axios.get(endpointURL('/users/achen041bd2/projects'))
+    .then(response => {
+      console.dir(response.data)
+      writeFile(fileURL, response.data)
+      res.send(response.data)
+    })
+    .catch(error => {
+      console.log('error', error)
+    })
 })
 
 app.get('/projects', function (req, res) {
