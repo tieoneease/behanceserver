031bfe33b437ed4961d49bc2aa7d99b37499139e
Sam Chung <tieoneease@gmail.com>
2017-03-16 07:27:37 -0400

    more cors

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var app = express()
 var axios = require('axios')
 var jsonfile = require('jsonfile')
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200);
