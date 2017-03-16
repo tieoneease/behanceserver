6c657f8f90b1f05eb733018ba0a76aa9c2de51a3
Sam Chung <tieoneease@gmail.com>
2017-03-16 07:24:58 -0400

    more cores

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var axios = require('axios')
 var jsonfile = require('jsonfile')
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
   next();
 });
 
